5c32f9496d10544e439f6860c0ecdc50faaf12b9
edanilkov <edanilkov@igatec.com>
2023-11-06 18:26:55 +0300

    create button

---
+++ src/components/buttons/Button/Button.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 65px;
+  padding: 42px 120px 42px 120px;
+  color: rgba(0, 45, 147, 0.8);
+  font-size: 18px;
+  letter-spacing: 1.08px;
+`
